329fd2d1f6346786076cb3a45db8df62411f5ffe
Aaron Howser <aaronhowser1@gmail.com>
2023-04-19 07:19:26 -0500

    4/5

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -4,15 +4,56 @@ import java.lang.IndexOutOfBoundsException
 
 data class Car(val id: String, val color: String)
 
-val parkingLot = mutableMapOf<Int,Car>()
+data class ParkingLot(val size: Int, val spots: MutableMap<Int, Car>) {
 
-const val PARKING_LOT_SIZE = 20
+    fun park(id: String, color: String) {
+
+        var i = 1
+        while (spots[i] != null) i++
+
+        if (i > size) {
+            println("Sorry, the parking lot is full.")
+        } else {
+           spots[i] = Car(id, color)
+            println("$color car parked in spot $i.")
+        }
+    }
+
+    fun leave(spot: Int) {
+        val occupied = check(spot) != null
+
+        if (occupied) {
+            spots.remove(spot)
+            println("Spot $spot is free.")
+        } else {
+            println("There is no car in spot $spot.")
+        }
+    }
+
+    fun printStatus() {
+
+        if (spots.isEmpty()) {
+            println("Parking lot is empty.")
+        } else {
+            for ((spotNumber, car) in spots) {
+                println("""
+                $spotNumber ${car.id} ${car.color}
+            """.trimIndent())
+            }
+        }
+    }
+
+    private fun check(spot: Int): Car? = spots[spot]
+}
 
 fun main() {
     showMenu()
 }
 
 fun showMenu() {
+
+    var parkingLot: ParkingLot? = null
+
     while (true) {
 
         val input = readln()
@@ -20,38 +61,28 @@ fun showMenu() {
 
         try {
             when (split.firstOrNull()) {
-                "leave" -> leave(split[1].toInt())
-                "park" -> park(split[1], split[2])
+                "create" -> {
+                    val amount = split[1].toInt()
+                    parkingLot = ParkingLot(amount, mutableMapOf())
+                    println("Created a parking lot with $amount spots.")
+                }
                 "exit" -> break
+                else -> {
+
+                    if (parkingLot == null) {
+                        println("Sorry, a parking lot has not been created.")
+                        continue
+                    }
+
+                    when (split.firstOrNull()) {
+                        "leave" -> parkingLot.leave(split[1].toInt())
+                        "park" -> parkingLot.park(split[1], split[2])
+                        "status" -> parkingLot.printStatus()
+                    }
+                }
             }
         } catch (e: IndexOutOfBoundsException) {
             println(e)
         }
     }
 }
-
-fun park(id: String, color: String) {
-
-    var i = 1
-    while (parkingLot[i] != null) i++
-
-    if (i > PARKING_LOT_SIZE) {
-        println("Sorry, the parking lot is full.")
-    } else {
-        parkingLot[i] = Car(id, color)
-        println("$color car parked in spot $i.")
-    }
-}
-
-fun leave(spot: Int) {
-    val occupied = check(spot) != null
-
-    if (occupied) {
-        parkingLot.remove(spot)
-        println("Spot $spot is free.")
-    } else {
-        println("There is no car in spot $spot.")
-    }
-}
-
-fun check(spot: Int): Car? = parkingLot[spot]
